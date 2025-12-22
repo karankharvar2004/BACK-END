8ecdf115d76943ab2e2177d6f903d6080fd5c2c5
karan-7006 <kharvarkaran21@gmail.com>
2025-12-22 10:44:28 +0530

    first commit

---
+++ Practice/Python/FruitKha-DJango Project/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
