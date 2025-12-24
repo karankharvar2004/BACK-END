1546440e2c2f1b32605822d83e1ff0d0c01a1757
karan-7006 <kharvarkaran21@gmail.com>
2025-12-24 16:20:12 +0530

    first commit

--- Practice/Python/FruitKha-DJango Project/myapp/urls.py
+++ Practice/Python/FruitKha-DJango Project/myapp/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('change-password/',views.change_password,name='change-password'),
+    path('forgot-password/',views.forgot_password,name='forgot-password'),
+
     
 ]
--- Practice/Python/FruitKha-DJango Project/mysite/settings.py
+++ Practice/Python/FruitKha-DJango Project/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,4 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kharvarkaran21@gmail.com'
+EMAIL_HOST_PASSWORD = 'iehe emzv zeal dfda'
 
