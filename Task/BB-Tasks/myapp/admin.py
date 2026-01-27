8bbbe7c24ddbcf3d36873cb5f38b297bb9bfa91c
KaranKharvar7006 <karankharvar2004@gmail.com>
2026-01-27 13:30:54 +0530

    first commit

--- Task/BB-Tasks/myapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-admin.site.site_header = "BrainyBeams Admin"
-admin.site.site_title = "BrainyBeams Portal"
-admin.site.index_title = "Welcome to BrainyBeams Dashboard"
