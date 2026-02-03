21885c1b488ab224044a9f216c42f3720a789bf1
KaranKharvar7006 <karankharvar2004@gmail.com>
2026-02-04 00:50:12 +0530

    first commit

---
+++ Practice/Python/List/SumOfN.py
@@ -0,0 +1,11 @@
+num = int(input("Enter Your value: "))
+
+sum = 0
+
+for i in range(0, num+1):
+    sum = sum + i
+print(sum)
+
+
+# total=num*(num+1)/2
+# print(total)
---
+++ Practice/Python/Simple/Example.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter Your First Number: "))
+# num2 = int(input("Enter Your Second Number: "))
+
+if num1 / 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
