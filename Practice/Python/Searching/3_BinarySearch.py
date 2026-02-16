303437ba21b049be16b3a6e1bd173cf2294cce33
KaranKharvar7006 <karankharvar2004@gmail.com>
2026-02-16 22:37:33 +0530

    first commit

---
+++ Practice/Python/Searching/3_BinarySearch.py
@@ -0,0 +1,24 @@
+# Binary search
+
+arr = [10, 20, 30, 40, 50, 60, 70]
+target = 40
+
+left = 0
+right = len(arr) - 1
+
+found = False
+
+while left <= right:
+    mid = (left + right) // 2
+    
+    if arr[mid] == target:
+        print("Element found at index:", mid)
+        found = True
+        break
+    elif arr[mid] < target:
+        left = mid + 1
+    else:
+        right = mid - 1
+
+if not found:
+    print("Element not found")
